525f9610849cd51661c7530ddd1175bccf33e020
Kwame Mintah <kwame_mintah@hotmail.co.uk>
2023-09-13 00:32:15 +0100

    feat(main): builtin terraform functions and terraformignore
    trying other the various builtin terraform functions and include a terraformignore for terraform cloud vcs

--- main.tf
+++ main.tf
@@ -13,3 +13,52 @@ variable "worlds" {
 variable "splat" {
   type = list(any)
 }
+
+variable "cartoons" {
+  type    = list(any)
+  default = ["Jetix", "Cartoon Network", "Boomerang"]
+}
+
+variable "memorable" {
+  type = map(any)
+  default = {
+    "Kids Next Door" = "1",
+    "Ben Ten"        = "Ben Tennyson",
+    "Power Rangers"  = "Red Ranger"
+  }
+}
+
+variable "memorable_objects" {
+  type = object({
+    KidsNextDoor = number
+    BenTen       = string
+    PowerRangers = string
+  })
+  default = {
+    "KidsNextDoor" = 1,
+    "BenTen"       = "Ben Tennyson",
+    "PowerRangers" = "Red Ranger"
+  }
+}
+
+variable "tuple" {
+  type    = tuple([string, number, bool])
+  default = ["One", 2, false] # Must match type order.
+}
+
+variable "abc" {
+  type    = string
+  default = "abc"
+}
+
+variable "null_items" {
+  type    = list(any)
+  default = [null, null, "", "last"]
+}
+
+variable "keys" {
+  type = map(any)
+  default = {
+    "Peanut" = "Butter"
+  }
+}
